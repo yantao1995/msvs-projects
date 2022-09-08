f963808471e124c29e13396c378fc32430ecbdb0
Tany <1173904005@qq.com>
2022-09-08 18:34:22 +0800

    update winmine

---
+++ winmine/winmineDlg.h
@@ -0,0 +1,43 @@
+﻿
+// winmineDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CwinmineDlg 对话框
+class CwinmineDlg : public CDialogEx
+{
+// 构造
+public:
+	CwinmineDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WINMINE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton4();
+	afx_msg void OnBnClickedButton5();
+	CListCtrl m_list_controller;
+	//bool  isFirst = true;
+	afx_msg void OnBnClickedButton6();
+};
